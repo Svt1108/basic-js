eb52fdf8e34e998e1cd01156e8e45a8a7137b6d9
Svetlana Pozina <cherryblack@list.ru>
2022-04-28 18:30:10 +0300

    feat: add get-email-domain

--- src/file-names.js
+++ src/file-names.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * There's a list of file, since two files cannot have equal names,
@@ -15,11 +15,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  let arr = [];
+  let temp;
+  for (let i = 0; i < names.length; i++) {
+    if (!arr.includes(names[i])) arr.push(names[i]);
+    else {
+      let j = 1;
+      while (1) {
+        temp = `${names[i]}(${j})`;
+        j++;
+        if (!arr.includes(temp)) {
+          arr.push(temp);
+          break;
+        }
+      }
+    }
+  }
+
+  return arr;
 }
 
 module.exports = {
-  renameFiles
+  renameFiles,
 };
+
+console.log(renameFiles(["file", "file", "image", "file(1)", "file"]));
